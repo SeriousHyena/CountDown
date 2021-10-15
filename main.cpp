82364acdf14f893ed5b957a19e098c64396bbd70
SeriousHyena <emackiii@gmail.com>
2021-10-15 13:06:46 -0700

    Added sumValues Function

--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 // declare functions here
 void countDownFrom(int num);
+int sumValues(int num);
 
 
 int main()
 {
 	countDownFrom(10);		//invoke the function here
+	int totalSum = sumValues(10);			//invoke the function
+	cout << "The sum is: " << totalSum << endl;
 
 	return 0;
 }
@@ -21,4 +24,14 @@ void countDownFrom(int num)
 		cout << num << endl;
 		countDownFrom(num - 1);		//recursively decrement the number in the function
 	}
+}
+
+int sumValues(int num)
+{
+	if (num > 1)
+	{
+		return num + sumValues(num - 1);	//need the -1 to implement the recursion
+	}
+
+	return num;
 }
